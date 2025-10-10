38b5a4b805d9eb111ca7ab875a42eb9fa6258df0
Bharti Tushar Kamaldeo <88471193+Tushar9898@users.noreply.github.com>
2025-10-10 17:45:22 +0530

    Time: 1258 ms (30.77%), Space: 23.5 MB (23.08%) - LeetHub

---
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maximumEnergy(self, energy, k):
+        """
+        :type energy: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(energy)
+        dp = [0] * n
+        ans = float('-inf')
+
+        for i in range(n - 1, -1, -1):
+            if i + k < n:
+                dp[i] = energy[i] + dp[i + k]
+            else:
+                dp[i] = energy[i]
+            ans = max(ans, dp[i])
+
+        return ans
