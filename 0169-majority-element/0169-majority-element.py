ff28d2d0746a0aafd73ccbf59540b6b50d8ed302
Bharti Tushar Kamaldeo <88471193+Tushar9898@users.noreply.github.com>
2026-01-10 22:12:26 +0530

    Time: 12 ms (41.14%), Space: 13.9 MB (15.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,12 @@
 class Solution(object):
     def majorityElement(self, nums):
-        nums.sort()
-        size=len(nums)
-        req_freq=size//2
-        ans=nums[0]
-        freq=1
-        if size==1:
-            return ans
-        for i in range(size):
-            if nums[i]==nums[i-1]:
-                freq=freq+1
+        freq=0
+        ans=0
+        for i in nums:
+            if freq==0:
+                ans=i
+            if ans==i:
+                freq+=1
             else:
-                freq=1
-                ans=nums[i]
-            if freq>req_freq:
-                return ans
+                freq-=1
+        return ans
